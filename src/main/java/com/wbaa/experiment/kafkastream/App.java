e807e537690cf3d677d99c1750c55174c3affe8c
Zitong Lian <zitong.lian@ing.com>
2018-12-03 12:05:34 +0100

    Add avro, mvn dependencies

---
+++ src/main/java/com/wbaa/experiment/kafkastream/App.java
@@ -0,0 +1,13 @@
+package com.wbaa.experiment.kafkastream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
